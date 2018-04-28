eaaad59c5c44ee45a672f407b4137e452b156b59
Juma Allan <allanjuma@gmail.com>
2018-04-28 15:47:03 +0300

    Login Page UI

--- app/src/main/java/com/androidstudy/adverticket/LoginActivity.kt
+++ app/src/main/java/com/androidstudy/adverticket/LoginActivity.kt
@@ -2,11 +2,16 @@ package com.androidstudy.adverticket
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Window
+import android.view.WindowManager
 
 class LoginActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        requestWindowFeature(Window.FEATURE_NO_TITLE)
+        window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN)
         setContentView(R.layout.activity_login)
     }
 }
